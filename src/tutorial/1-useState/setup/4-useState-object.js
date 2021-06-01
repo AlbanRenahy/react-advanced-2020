732fdded0330d863d678974d7a2bb5822a624d49
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-06-01 06:56:45 +0200

    useState - Object example

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const changeMessage = () => {
+    /**
+     * For example, if you're using the object, then you need to make sure that you always, always use the spread operator.Or there's also some other older setup where essentially you copy the old values from the state value and then you just decide which one you would want to choose.
+     */
+    if (person.message === "random message") {
+      setPerson({ ...person, message: "hello world" });
+    } else {
+      setPerson({ ...person, message: "random message" });
+    }
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
